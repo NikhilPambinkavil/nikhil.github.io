2dc9d2a616b8c2a4fde0f2c3f13b100329be4843
NikhilPambinkavil <nkp25xdz@bangor.ac.uk>
2025-04-07 16:11:39 +0100

    Fixed Error

--- Xreality/test/script.js
+++ Xreality/test/script.js
@@ -1,59 +1,55 @@
 window.onload = () => {
     let places = staticLoadPlaces();
-    renderPlaces(places);
+    if (places.length > 0) {
+        renderPlaces(places);
+    } else {
+        console.error("No locations found!");
+    }
 };
 
 function staticLoadPlaces() {
-   return [
-       {
-           name: 'Magnemite',
-           location: {
-               lat: 53.2278412,
-               lng: -4.1302649,
-           }
-       },
-       {
-        name: 'pointio',
-        location: {
-            lat: 53.2277804,
-            lng: -4.1302079,
-        }
-       },
-       {
-        name: 'pl4',
-        location: {
-            lat: 53.2277437,
-            lng: -4.1302056,
-        }
+    return [
+        {
+            name: 'Magnemite',
+            location: { lat: 53.2278412, lng: -4.1302649 }
+        },
+        {
+            name: 'pointio',
+            location: { lat: 53.2277804, lng: -4.1302079 }
+        },
+        {
+            name: 'pl4',
+            location: { lat: 53.2277437, lng: -4.1302056 }
         },
         {
             name: 'pl4.5',
-            location: {
-                lat: 53.2278414,
-                lng: -4.1303702,
-            }
+            location: { lat: 53.2278414, lng: -4.1303702 }
         },
-   ];
+    ];
 }
 
 function renderPlaces(places) {
-   let scene = document.querySelector('a-scene');
+    let scene = document.querySelector('a-scene');
 
-   places.forEach((place) => {
-       let latitude = place.location.lat;
-       let longitude = place.location.lng;
+    if (!scene) {
+        console.error("A-Frame scene not found!");
+        return;
+    }
 
-       let model = document.createElement('a-entity');
-       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       model.setAttribute('gltf-model', 'https://nikhilpambinkavil.github.io/nikhil.github.io/Xreality/tools/scene.gltf');
-       model.setAttribute('rotation', '0 180 0');
-       model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '0.5 0.5 0.5');
+    places.forEach((place) => {
+        let { lat, lng } = place.location;
 
-       model.addEventListener('loaded', () => {
-           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-       });
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${lng};`);
+        model.setAttribute('gltf-model', 'https://nikhilpambinkavil.github.io/nikhil.github.io/Xreality/tools/scene.gltf');
+        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('scale', '0.5 0.5 0.5');
 
-       scene.appendChild(model);
-   });
+        model.addEventListener('loaded', () => {
+            console.log(`Model loaded at: ${lat}, ${lng}`);
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'));
+        });
+
+        scene.appendChild(model);
+    });
 }
